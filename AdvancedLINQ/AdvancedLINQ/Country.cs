279ad16408507e3246a81aba70561f58728d24aa
Ecat3rina <57297182+Ecat3rina@users.noreply.github.com>
2020-02-27 17:11:05 +0200

    AdvancedLINQAddedinSource

---
+++ AdvancedLINQ/AdvancedLINQ/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedLINQ
+{
+    public class Country
+    {
+        public IEnumerable<Nationality> Nationalities { get; set; }
+        public string Name { get; set; }
+        public int Area { get; set; }
+        public int Population { get; set; }
+    }
+}
---
+++ AdvancedLINQ/AdvancedLINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvancedLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countries = new Country[]
+            {
+                new Country {Name = "Moldova", Area = 200, Population = 100},
+                new Country {Name = "China", Area = 2005, Population = 1001},
+            };
+            IEnumerable<>
+            var new_countries=new Country();
+        }
+    }
+}
